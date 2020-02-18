8c46d0b495e1d3ff9f3d521d88f90ccd48975075
Hyuain <Hyuain97@163.com>
2020-02-18 21:13:03 +0800

    初始化 Redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {renderRoutes, RouteConfig} from 'react-router-config';
 import {HashRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
 import routes from './routes';
+import store from './store';
 import {GlobalStyle} from './style';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store}>
       <HashRouter>
+        <GlobalStyle></GlobalStyle>
         {renderRoutes(routes as RouteConfig[])}
       </HashRouter>
-      <GlobalStyle></GlobalStyle>
-    </div>
+    </Provider>
   );
 }
 
