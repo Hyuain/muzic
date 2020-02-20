f7ad9b7d7b1e1223a06731a64ce4d15c53da4bfd
Hyuain <Hyuain97@163.com>
2020-02-20 19:37:14 +0800

    Updates

---
+++ precache-manifest.017cd72ff53d8ae5be3f79185abff835.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb6ebaf900934e6ea521731c0e5f4ee",
+    "url": "/muzic/index.html"
+  },
+  {
+    "revision": "14464b022290182417ca",
+    "url": "/muzic/static/css/2.6bf38e3a.chunk.css"
+  },
+  {
+    "revision": "14464b022290182417ca",
+    "url": "/muzic/static/js/2.a0f43e28.chunk.js"
+  },
+  {
+    "revision": "7e7c70b1fda7b156ad8e1426f8d2e524",
+    "url": "/muzic/static/js/2.a0f43e28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b30cf8e00d6ecda5a89",
+    "url": "/muzic/static/js/main.b68f64e1.chunk.js"
+  },
+  {
+    "revision": "f6581cd4ca6baee4e42e",
+    "url": "/muzic/static/js/runtime-main.bf3b8203.js"
+  },
+  {
+    "revision": "548323d84498fd288657e0c1c684adfe",
+    "url": "/muzic/static/media/back.548323d8.svg"
+  },
+  {
+    "revision": "9003fe5821a446f98051de50957e037a",
+    "url": "/muzic/static/media/headphone.9003fe58.svg"
+  },
+  {
+    "revision": "bcaac69a1611cab1b65c3d2af005a586",
+    "url": "/muzic/static/media/menu.bcaac69a.svg"
+  },
+  {
+    "revision": "64444b39f93291cc186672eca04e6a7f",
+    "url": "/muzic/static/media/music.64444b39.png"
+  },
+  {
+    "revision": "2665087954f39fde671bcd5cbb28e2ae",
+    "url": "/muzic/static/media/search.26650879.svg"
+  }
+]);
