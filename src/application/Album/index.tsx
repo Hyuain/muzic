2937edbb685253e4d9ac49cff832e6feed43d5d7
Hyuain <Hyuain97@163.com>
2020-02-20 17:12:17 +0800

    增加 Album 切入动画

--- src/application/Album/index.tsx
+++ src/application/Album/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {RouteComponentProps} from 'react-router-dom';
 import {Container} from './style';
+import {CSSTransition} from 'react-transition-group';
 
-const Album = () => {
-  console.log('run');
+interface IAlbumProps extends RouteComponentProps{
+
+}
+
+const Album:React.FunctionComponent<IAlbumProps> = (props) => {
+  const [showStatus, setShowStatus] = useState<boolean>(true);
+
   return (
-    <Container>
-    </Container>
-  )
+    <CSSTransition
+      in={showStatus}
+      timeout={300}
+      classNames="fly"
+      appear={true}
+      unmountOnExit
+      onExited={props.history.goBack}
+    >
+      <Container>
+        hahahah
+      </Container>
+    </CSSTransition>
+  );
 };
 
-export default React.memo(Album)
+export default React.memo(Album);
